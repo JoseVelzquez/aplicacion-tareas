0b8c109cc21baad2d4f993168967a10707341e17
JoseDev <josevelazquezolvera2@gmail.com>
2024-07-17 14:15:23 -0600

    segundo_commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
@@ -7,10 +6,6 @@ function App() {
 
   return (
     <div className='aplicacion-tareas'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img src={freeCodeCampLogo} 
-        className='freecodecamp-logo' />
-      </div>
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
         <ListaDeTareas />
